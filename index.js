56509e12b6bb4aaba5faa95526eab37de26dfcfe
Alan <alanjurnetb@gmai.com>
2017-02-10 10:26:13 -0300

    test

--- index.js
+++ index.js
@@ -16,3 +16,17 @@ app.listen(app.get('port'), function() {
 });
 
 
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.USERS_DB_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT * FROM employees;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
