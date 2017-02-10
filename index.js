9ee1c292a18363380ec45ed8caf6bb3e4d7bf467
Alan <alanjurnetb@gmai.com>
2017-02-10 10:01:45 -0300

    mod

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.logger());
+
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function(request, response) {
@@ -13,3 +14,5 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
